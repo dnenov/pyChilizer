b2d0ad1ded5b80b102d916d08d54a07d78576f5b
rastermadre <daria.ivanciucova@gmail.com>
2020-09-18 15:11:59 +0100

    Updated Removed Unplaced Rooms
    Now removed only unplaced rooms (previously removed unenclosed too)

--- pyChilizerExtension/pyChilizerExtension.extension/pyChilizer.tab/Tiny Tools.panel/Remove Unplaced Rooms.pushbutton/script.py
+++ pyChilizerExtension/pyChilizerExtension.extension/pyChilizer.tab/Tiny Tools.panel/Remove Unplaced Rooms.pushbutton/script.py
@@ -1,13 +1,7 @@
 __title__= "Remove \nUnplaced Rooms"
 __doc__ = "Delete unplaced rooms from project"
 
-# pylint: disable=invalid-name,import-error,superfluous-parens
-
 from pyrevit import revit, DB
-from pyrevit import script
-from pyrevit import framework
-
-output = script.get_output()
 
 # collect
 with revit.Transaction("Remove Unplaced Rooms"):
@@ -16,11 +10,15 @@ with revit.Transaction("Remove Unplaced Rooms"):
         .WhereElementIsNotElementType() \
         .ToElements()
 
-    unplaced_ids = []
-    for r in rooms:
-        room_areas = r.Parameter[DB.BuiltInParameter.ROOM_AREA]
-        if room_areas.AsDouble() == 0:
-            unplaced_ids.append(r.Id)
+    # get ids of rooms with location equal to None
+    unplaced_ids = [r.Id for r in rooms if r.Location == None]
 
-    # remove
-    revit.doc.Delete(framework.List[DB.ElementId](unplaced_ids))
+    deleted = [] # to keep track of elements deleted
+
+    # remove unplaced rooms
+    for upid in unplaced_ids:
+        revit.doc.Delete(upid)
+        deleted.append(upid)
+
+    # print result
+    print("Removed {0} unplaced rooms".format(len(deleted)))
