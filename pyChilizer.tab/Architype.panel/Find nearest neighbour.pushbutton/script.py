d27208c4586b6f6fd60b2be5fbf60cd2cc15b271
2adicted <dido.nenov@gmail.com>
2020-09-24 16:33:36 +0100

    Works for all Windows. Works

--- pyChilizer.tab/Architype.panel/Find nearest neighbour.pushbutton/script.py
+++ pyChilizer.tab/Architype.panel/Find nearest neighbour.pushbutton/script.py
@@ -1,11 +1,8 @@
 __title__ = "Find nearest\n neighbour"
 __doc__ = ""
 
-import re
-
 from pyrevit import revit, DB
 from pyrevit.framework import List
-from pyrevit import coreutils
 
 
 def CreateLine(doc, line, dir):
@@ -39,11 +36,6 @@ def FindSingleIntersection(_win, _host_win, _current_view, _location_pt, _direct
 
     nearest_up = ref_int.FindNearest(_location_pt, _direction)     # Find only the nearest
     shot = nearest_up.GetReference()    # We got a shot! TO DO: Check if no intersection
-
-    # selection = revit.get_selection()
-    #
-    # element_ids = [shot.ElementId]
-    # selection.set_to(element_ids)
 
     return shot
 
@@ -109,99 +101,15 @@ elements = DB.FilteredElementCollector(revit.doc) \
 windows = DB.FilteredElementCollector(revit.doc) \
     .OfCategory(DB.BuiltInCategory.OST_Windows) \
     .WhereElementIsNotElementType() \
-    .ToElementIds()
+    .ToElements()
 
-# filter nested by not hosted on wall - MAYBE BETTER FILTER - I think that's great!
-# nested_win = [w for w in windows if not isinstance(w.Host, DB.Wall)]
-# host_wall = [w.Host for w in windows if isinstance(w.Host, DB.Wall)]
-
-win = revit.pick_element("pick a window") # Keep for visual debugging
-
-# with revit.Transaction("populate parameter"):
-    # param = win.LookupParameter("ART_ThermalBridgeValue_Right")
-    # param = win.LookupParameter("ART_ThermalBridgeValue_Left")
-    # param.Set(7)
-
-PopulateIntersection(win, current_view)
-
-# for win in nested_win:
+# filter nested by not hosted on wall - MAYBE BETTER FILTER
+nested_win = [w for w in windows if not isinstance(w.Host, DB.Wall)]
+host_wall = [w.Host for w in windows if isinstance(w.Host, DB.Wall)]
 
 
+# win = revit.pick_element("pick a window") # Keep for visual debugging
 
-# nearest_up = ref_int.Find(location_pt, direction_up)
-# ups = []
-# try:
-#     for nu in set(nearest_up):
-#         i_up = revit.doc.GetElement(nu.GetReference())
-#         ups.append(i_up)
-#         print("UP      {0}, Element Id: {1}".format(i_up.Name, i_up.Id))
-# except:
-#     pass
-#
-# nearest_down = ref_int.Find(location_pt, direction_down)
-# downs = []
-# try:
-#     for nd in set(nearest_down):
-#         i_dn = revit.doc.GetElement(nd.GetReference())
-#         nd.append(i_dn)
-#         print("DOWN: {0}, Element Id: {1}".format(i_dn.Name, i_dn.Id))
-# except:
-#     pass
-
-# ref = ref_context.GetReference()
-#
-# selection = revit.get_selection()
-# selection.set_to(list(ref))
-
-# for win in nested_win:
-#
-#     print ("Intersecting family type name: {0} Element Id {1}".format(win.Name, win.Id))
-#
-#     class_filter = DB.ElementClassFilter(DB.FamilyInstance)
-#     target = DB.FindReferenceTarget.Face
-#     ref_int = DB.ReferenceIntersector(class_filter, target, curview)
-#
-#     bb = win.get_BoundingBox(curview)
-#     x = win.Location.Point.X
-#     y = win.Location.Point.Y
-#     location_pt = DB.XYZ(x, y, bb.Max.Z)
-#
-#     direction_down = DB.XYZ(0, 0, -1)
-#     direction_up = DB.XYZ(0, 0, 1)
-#
-#     nearest_up = ref_int.Find(location_pt, direction_up)
-#     ups = []
-#     try:
-#         for nu in set(nearest_up):
-#             i_up = revit.doc.GetElement(nu.GetReference())
-#             ups.append(i_up)
-#             print("UP      {0}, Element Id: {1}".format(i_up.Name, i_up.Id))
-#     except:
-#         pass
-#
-#     nearest_down = ref_int.Find(location_pt, direction_down)
-#     downs = []
-#     try:
-#         for nd in set(nearest_down):
-#             i_dn = revit.doc.GetElement(nd.GetReference())
-#             nd.append(i_dn)
-#             print("DOWN: {0}, Element Id: {1}".format(i_dn.Name, i_dn.Id))
-#     except:
-#         pass
-
-
-# illustrate with line
-#    line = DB.Line.CreateBound(bb.Max, direction)
-
-#    x = bb.Max.X
-##    y = bb.Min.Y
-#    z = bb.Max.Z
-
-#    normal = DB.XYZ(x,y,z)
-
-#   origin = center
-
-#    with revit.Transaction("draw line"):
-#        plane = DB.Plane.CreateByNormalAndOrigin(normal, origin)
-#        sketch_plane = DB.SketchPlane.Create(revit.doc, plane)
-#       model_line = revit.doc.Create.NewModelCurve(line, sketch_plane)
+with revit.TransactionGroup('Populate Thermal Bridge Values'):
+    for win in nested_win:
+        PopulateIntersection(win, current_view)
