6623c022ca165ae9071f5f8ce92b1fcdf9aa1d91
rastermadre <daria.ivanciucova@gmail.com>
2020-10-02 17:21:33 +0100

    Ungroup button update
    added disclaimer before proceeding to ungroup

--- pyChilizer.tab/Tiny Tools.panel/Cleanup Tools.pulldown/Ungroup All.pushbutton/script.py
+++ pyChilizer.tab/Tiny Tools.panel/Cleanup Tools.pulldown/Ungroup All.pushbutton/script.py
@@ -3,20 +3,30 @@ __doc__ = "Ungroups all groups in project"
 
 from pyrevit import revit, DB, script, forms
 
-output = script.get_output()
-# Gather groups
-coll_groups = DB.FilteredElementCollector(revit.doc) \
-    .OfClass(DB.Group) \
-    .WhereElementIsNotElementType() \
-    .ToElements()
-ungr_ids=[]
-if coll_groups:
-    with revit.Transaction ("Ungroup All", revit.doc):
-        print("UNGROUPED {} GROUPS:".format(len(coll_groups)))
-        for gr in coll_groups:
-            ungrouped = (gr.UngroupMembers())
-            print ("\n")
-            print("{} \t {} ".format(gr.Name,output.linkify(gr.Id)))
-else:
-    print("No groups in project")
+# disclaimer warning
+if forms.alert(msg="Are you sure?",\
+               sub_msg="Proceeding will ungroup all groups in the model.",\
+               ok=True, cancel=True,\
+               warn_icon=True):
+
+    output = script.get_output()
+
+    # Gather groups
+    coll_groups = DB.FilteredElementCollector(revit.doc) \
+        .OfClass(DB.Group) \
+        .WhereElementIsNotElementType() \
+        .ToElements()
+
+    ungr_ids=[]
+
+    if coll_groups:
+        with revit.Transaction ("Ungroup All", revit.doc):
+            print("UNGROUPED {} GROUPS:".format(len(coll_groups)))
+            for gr in coll_groups:
+                # Iterate through groups and ungroup
+                ungrouped = (gr.UngroupMembers())
+                print ("\n")
+                print("{} \t {} ".format(gr.Name,output.linkify(gr.Id)))
+    else:
+        print("No groups in project")
 
