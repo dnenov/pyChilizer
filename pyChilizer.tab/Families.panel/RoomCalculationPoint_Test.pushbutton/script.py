fcd65cac4dd1d60730424b7fe45ebb0a1d319009
2adicted <dido.nenov@gmail.com>
2021-09-10 17:08:54 +0100

    Added Purge pulldown (empty)

---
+++ pyChilizer.tab/Families.panel/RoomCalculationPoint_Test.pushbutton/script.py
@@ -0,0 +1,10 @@
+__title__ = "Room 3"
+__doc__ = "Add Room Calculation Point to multiple Families \nv1.0"
+
+from pyrevit import revit, DB, HOST_APP, UI
+from pyrevit.framework import List
+from pyrevit.forms import ProgressBar
+
+
+print("Test 1 .. 2 .. 3")
+
