7807ac75aa81de77275c4aac875779009aa594d6
2adicted <dido.nenov@gmail.com>
2021-09-10 15:43:16 +0100

    pyChilizer_restart

--- Architype.tab/Help.panel/Help.pushbutton/script.py
+++
@@ -1,7 +0,0 @@
-__title__ = "Architype\nFAQ"
-__doc__ = "Architype Ribbon Frequently Asked Qestions \nv1.0"
-
-from pyrevit import revit, DB
-from pyrevit.framework import List
-from pyrevit.forms import ProgressBar
-
