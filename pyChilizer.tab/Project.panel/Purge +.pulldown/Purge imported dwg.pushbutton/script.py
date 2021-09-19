2909f521476ed56e19800b27078e6e1d32f5e3de
2adicted <dido.nenov@gmail.com>
2021-09-19 20:52:32 +0100

    Added purge imported patterns and unused Sheets

--- pyChilizer.tab/Project.panel/Purge +.pulldown/Purge imported dwg.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/Purge +.pulldown/Purge imported dwg.pushbutton/script.py
@@ -1,5 +1,5 @@
 __title__ = "Purge imported dwg"
-__doc__ = "Delete all the pesky imported dwg from the Project. All of them."
+__doc__ = "Delete all the imported dwg from the Project. All of them."
 
 from pyrevit import forms
 from pyrevit import revit, DB
@@ -8,17 +8,19 @@ from pyrevit.framework import List
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
 
-
 cl = DB.FilteredElementCollector(doc)
 
 cad_imports = List[DB.ElementId]([i.Id for i in cl.OfClass(DB.ImportInstance) if not i.IsLinked])
 
 message = 'There are {} cad imports in the model. Are you sure you want to delete them?'.format(str(len(cad_imports)))
 
-if forms.alert(message, ok=False, yes=True, no=True, exitscript=True):
-    with revit.Transaction("Remove dwg imports"):
-        for id in cad_imports:
-            doc.Delete(cad_imports)
+if len(cad_imports) == 0:
+    forms.alert("No Imports, well done!")
+else:
+    if forms.alert(message, ok=False, yes=True, no=True, exitscript=True):
+        with revit.Transaction("Remove dwg imports"):
+            for id in cad_imports:
+                doc.Delete(cad_imports)
 
 
 
