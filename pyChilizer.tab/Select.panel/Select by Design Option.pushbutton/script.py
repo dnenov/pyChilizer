78fc3937461c786cc141c71cd96c7133f630813d
rastermadre <daria.ivanciucova@gmail.com>
2021-09-21 15:42:51 +0100

    Updated Select by Design Option

--- pyChilizer.tab/Select.panel/Select by Design Option.pushbutton/script.py
+++ pyChilizer.tab/Select.panel/Select by Design Option.pushbutton/script.py
@@ -1,19 +1,22 @@
-__title__ = "Select by\nDesign Option"
-__doc__ = "All elements from a Design Option. Useful before deleting the design option."
+__title__ = "Select by Design Option"
+__doc__ = "Select elements of a design option"
 
-from pyrevit import revit, DB, UI
+
+from pyrevit import revit, DB, UI, forms
 from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button)
 import sys
 
 
 def get_full_option_name(design_option):
-    do_set = design_option.get_Parameter(DB.BuiltInParameter.DESIGN_OPTION_PARAM).AsString()
+    do_set_id = design_option.get_Parameter(DB.BuiltInParameter.OPTION_SET_ID).AsElementId()
+    do_set = revit.doc.GetElement(do_set_id).Name
     do_name = design_option.Name
     do_full_name = " : ".join([do_set, do_name])
     return do_full_name
 
-design_options = DB.FilteredElementCollector(revit.doc).OfClass(DB.DesignOption).ToElements()
 
+design_options = DB.FilteredElementCollector(revit.doc).OfClass(DB.DesignOption).ToElements()
+forms.alert_ifnot(design_options, "No Design Options in model.", exitscript=True)
 do_dict = {get_full_option_name(do) : do for do in design_options}
 
 # construct rwp UI
@@ -36,6 +39,8 @@ selSet = []
 
 do_filter = DB.ElementDesignOptionFilter(chosen_do.Id)
 do_el_list = DB.FilteredElementCollector(revit.doc).WherePasses(do_filter).ToElementIds()
+
+forms.alert_ifnot(do_el_list, "No elements in Design Option:\n {}".format(chosen_do.Name), exitscript=True)
 
 for elid in do_el_list:
     selSet.append(elid)
