43bd43895913b95f3a41b5e75ec5d628a1d3dbb2
rastermadre <daria.ivanciucova@gmail.com>
2021-09-30 14:46:29 +0100

    List line styles
    sort by name, sort scale

--- pyChilizer.tab/Project.panel/Template Tools.pulldown/List Line Styles.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/Template Tools.pulldown/List Line Styles.pushbutton/script.py
@@ -2,7 +2,7 @@
 
 from pyrevit import revit, DB, forms
 from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button)
-
+from collections import OrderedDict
 
 # pick text style
 txt_types = DB.FilteredElementCollector(revit.doc).OfClass(DB.TextNoteType)
@@ -21,12 +21,20 @@ chosen_text_style = form.values["textstyle_combobox1"]
 cat = revit.doc.Settings.Categories.get_Item(DB.BuiltInCategory.OST_Lines)
 subcats = [subcat for subcat in cat.SubCategories]
 
+# create ordered dictionary
+unsorted_dict = {}
+for sc in subcats:
+    unsorted_dict[sc] = sc.Name
+
+sorted_subcats = OrderedDict(sorted(unsorted_dict.items(), key=lambda t:t[1]))
+
+
 view = revit.active_view
 # TODO: filter only relevant views
 
 # dims and scale
-scale = 1
-w = 6.25 * scale
+scale = float(view.Scale)/ 200
+w = 8 * scale
 text_offset = 1 * scale
 shift = 5 * scale
 
@@ -38,7 +46,7 @@ p2 = DB.XYZ(w, 0, 0)
 l1 = DB.Line.CreateBound(p1, p2)
 
 with revit.Transaction("Draw Lines"):
-    for ls in subcats:
+    for ls in sorted_subcats.keys():
 
         t1 = DB.Transform.CreateTranslation(DB.XYZ(0, -shift, 0))
         l1 = l1.CreateTransformed(t1)
@@ -46,7 +54,7 @@ with revit.Transaction("Draw Lines"):
         gs = ls.GetGraphicsStyle(DB.GraphicsStyleType.Projection)
         new_line.LineStyle = gs
 
-        label_text = ls.Name
+        label_text = sorted_subcats[ls]
         t2 = DB.Transform.CreateTranslation(DB.XYZ(text_offset, 0, 0))
         text_position = l1.CreateTransformed(t2).GetEndPoint(1)
 
