dc93bb983236d28a996561e82d047aac6086edb8
rastermadre <daria.ivanciucova@gmail.com>
2021-10-01 11:17:00 +0100

    added two new commands, reformat

--- pyChilizer.tab/Annotate.panel/legends.stack/Legend from filters.pushbutton/script.py
+++ pyChilizer.tab/Annotate.panel/legends.stack/Legend from filters.pushbutton/script.py
@@ -1,3 +1,5 @@
+"""Draw Legend based on View Filters of a chosen view"""
+
 from pyrevit import revit, DB, UI, forms
 from collections import defaultdict
 from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button)
@@ -20,23 +22,9 @@ def translate_rectg_vert (rec, vert_offset):
     return [cv.CreateTransformed(transform) for cv in rec]
 
 
-def draw_rectangle(y_offset, fill_type, view):
-    # draw a filled region of any type with a specified y offset
-    rectangle_outlines = translate_rectg_vert(orig_rectangle, y_offset)
-    crv_loop = DB.CurveLoop.Create(List[DB.Curve](rectangle_outlines))
-    new_reg = DB.FilledRegion.Create(revit.doc, fill_type.Id, view.Id, [crv_loop])
-    return new_reg
-
-
 def any_fill_type():
     # get any Filled Region Type
     return DB.FilteredElementCollector(revit.doc).OfClass(DB.FilledRegionType).FirstElement()
-
-
-def get_any_text_type_id():
-    # get a default text note type - to replace later
-    txt_type = revit.doc.GetElement(revit.doc.GetDefaultElementTypeId(DB.ElementTypeGroup.TextNoteType))
-    return txt_type.Id
 
 
 col_views = DB.FilteredElementCollector(revit.doc).OfClass(DB.View).WhereElementIsNotElementType()
@@ -44,42 +32,50 @@ col_views = DB.FilteredElementCollector(revit.doc).OfClass(DB.View).WhereElement
 # TODO: Sort out views with no filters
 # prep options
 view_dict1 = {v.Name: v for v in col_views}
-#gm_dict2 = {p.Definition.Name: p for p in gm_params_area}
 
-
-forms.select_views(title="Select Target View", multiple=False)
+# get all text styles to choose from
+txt_types = DB.FilteredElementCollector(revit.doc).OfClass(DB.TextNoteType)
+text_style_dict= {txt_t.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString(): txt_t for txt_t in txt_types}
 
 # construct rwp UI
 components = [
     Label("Pick Source View:"),
-    ComboBox(name="view_combobox1", options=view_dict1),
-    Button("Select")]
-form = FlexForm("Pick Source View", components)
+    ComboBox(name="view_combobox", options=view_dict1),
+    Label("Pick Text Style"),
+    ComboBox(name="textstyle_combobox", options=text_style_dict),
+    Label("Pick Colour Source"),
+    ComboBox(name="coloursource_combobox", options=["Cut", "Projection"]),
+    Button("Select")
+]
+form = FlexForm("Select", components)
 form.show()
 # assign chosen parameters
-src_view = form.values["view_combobox1"]
+src_view = form.values["view_combobox"]
+text_style = form.values["textstyle_combobox"]
+colour_source = form.values["coloursource_combobox"]
 
-# TODO: select target view
 legend_view = revit.active_view
 view_filters = src_view.GetFilters()
 
 legend_od = OrderedDict()
 
 for f in view_filters:
-#    print (src_view.GetFilterOverrides(f))
+
     overrides = src_view.GetFilterOverrides(f)
-    foreground_colour = overrides.SurfaceForegroundPatternColor
+    if colour_source == "Projection":
+        filter_colour = overrides.SurfaceForegroundPatternColor
+    else:
+        filter_colour = overrides.CutForegroundPatternColor
     filter_name = revit.doc.GetElement(f).Name
-    legend_od[filter_name] = foreground_colour
+    legend_od[filter_name] = filter_colour
 
 
 # dims and scale
-scale = 1
-w = 8 * scale
-h = 4 * scale
+scale = float(legend_view.Scale)/100
+w = 3.25 * scale
+h = 1.3 * scale
 text_offset = 1 * scale
-shift = 6 * scale
-h_offset = 10*scale
+shift = 2.32 * scale
 # create rectrangle
 crv_loop = DB.CurveLoop()
 
@@ -94,35 +90,32 @@ l2 = DB.Line.CreateBound(p2, p3)
 l3 = DB.Line.CreateBound(p3, p4)
 l4 = DB.Line.CreateBound(p4, p1)
 
-orig_rectangle = [l1, l2, l3, l4]
+rectangle = [l1, l2, l3, l4]
 
 offset = 0
-origin = DB.XYZ(0,shift,0)
+origin = DB.XYZ(0,0,0)
 
-#print (legend_od)
 
-with revit.Transaction("do smth"):
+with revit.Transaction("Draw Legend"):
     for v_filter in legend_od:
 
-        print (v_filter)
+        t1 = DB.Transform.CreateTranslation(DB.XYZ(0, -shift, 0))
+        rectangle = [line.CreateTransformed(t1) for line in rectangle]
+        crv_loop = DB.CurveLoop.Create(List[DB.Curve](rectangle))
+
         # draw rectangles with filled region
-        new_reg = draw_rectangle(offset, any_fill_type(),legend_view)
+        new_reg = DB.FilledRegion.Create(revit.doc, any_fill_type().Id, legend_view.Id, [crv_loop])
+
         # override fill and colour
         ogs = DB.OverrideGraphicSettings()
         colour = legend_od[v_filter]
-        print ("Colour", colour)
         ogs.SetSurfaceForegroundPatternColor(legend_od[v_filter])
         ogs.SetSurfaceForegroundPatternId(get_solid_fill_pat().Id)
         legend_view.SetElementOverrides(new_reg.Id, ogs)
 
         # place text next to filled regions
-        label_position = DB.XYZ(w+text_offset, -(offset-h), 0)
+        t2 = DB.Transform.CreateTranslation(DB.XYZ(text_offset, 0, 0))
+        label_position = rectangle[1].CreateTransformed(t2).GetEndPoint(1)
         label_txt = str(v_filter)
-        text_note = DB.TextNote.Create(revit.doc, legend_view.Id, label_position, label_txt, get_any_text_type_id())
-
-        # keep offsetting y
-        offset += shift
-        # offset again before starting a new group
-        origin = DB.XYZ(origin.X, -(offset), origin.Z)
-        offset += shift
+        text_note = DB.TextNote.Create(revit.doc, legend_view.Id, label_position, label_txt, text_style.Id)
 
--- pyChilizer.tab/Project.panel/Template Tools.pulldown/List Text Styles.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/Template Tools.pulldown/List Text Styles.pushbutton/script.py
@@ -1,53 +1,30 @@
-"""List Line Styles"""
+"""List Text Styles"""
 
 from pyrevit import revit, DB, forms
 from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button)
-
+from collections import OrderedDict
 
 # pick text style
 txt_types = DB.FilteredElementCollector(revit.doc).OfClass(DB.TextNoteType)
-text_style_dict= {txt_t.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString(): txt_t for txt_t in txt_types}
-# construct rwp UI
-components = [
-    Label("Pick Text Style:"),
-    ComboBox(name="textstyle_combobox1", options=text_style_dict),
-    Button("Select")]
-form = FlexForm("Appearance", components)
-form.show()
-# assign chosen values
-chosen_text_style = form.values["textstyle_combobox1"]
+text_style_dict= {txt_t: txt_t.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString() for txt_t in txt_types}
 
-
-cat = revit.doc.Settings.Categories.get_Item(DB.BuiltInCategory.OST_Lines)
-subcats = [subcat for subcat in cat.SubCategories]
-
+# sort styles by name
+sorted_text_styles = OrderedDict(sorted(text_style_dict.items(), key=lambda t:t[1]))
 view = revit.active_view
-# TODO: filter only relevant views
 
 # dims and scale
-scale = 1
-w = 6.25 * scale
-text_offset = 1 * scale
+scale = float(view.Scale)/ 200
 shift = 5 * scale
+offset = 0
 
+text_height = 0
+with revit.Transaction("Place Text Notes"):
+    for ts in sorted_text_styles:
 
-p1 = DB.XYZ(0, 0, 0)
-p2 = DB.XYZ(w, 0, 0)
+        label_text = sorted_text_styles[ts]
 
-# create lines between points
-l1 = DB.Line.CreateBound(p1, p2)
-
-with revit.Transaction("Draw Lines"):
-    for ls in subcats:
-
-        t1 = DB.Transform.CreateTranslation(DB.XYZ(0, -shift, 0))
-        l1 = l1.CreateTransformed(t1)
-        new_line = revit.doc.Create.NewDetailCurve (view, l1)
-        gs = ls.GetGraphicsStyle(DB.GraphicsStyleType.Projection)
-        new_line.LineStyle = gs
-
-        label_text = ls.Name
-        t2 = DB.Transform.CreateTranslation(DB.XYZ(text_offset, 0, 0))
-        text_position = l1.CreateTransformed(t2).GetEndPoint(1)
-
-        text_note = DB.TextNote.Create(revit.doc, view.Id, text_position, label_text, chosen_text_style.Id)
+#        print (sorted_text_styles[ts],text_height*304, offset )
+        text_position = DB.XYZ(0, -offset,0)
+        text_height = ts.get_Parameter(DB.BuiltInParameter.TEXT_SIZE).AsDouble()
+        offset += (text_height * 2.75 * float(view.Scale))
+        text_note = DB.TextNote.Create(revit.doc, view.Id, text_position, label_text, ts.Id)
