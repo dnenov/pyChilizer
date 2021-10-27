b438d44be04ed66179744902c6d8cbd96bd19e00
2adicted <dido.nenov@gmail.com>
2021-10-27 19:10:07 +0100

    Slack libraries now inside the extensions folder

--- hooks/doc-syncing.py
+++ hooks/doc-syncing.py
@@ -1,24 +1,23 @@
 #! python3
-import slack
+from pyrevit import HOST_APP, EXEC_PARAMS
+from pyrevit import revit, script
+import hooks_logger as hl
 
-# from pyrevit import HOST_APP, EXEC_PARAMS
-# from pyrevit import revit, script
-# import hooks_logger as hl
+import slack 
 
+args = EXEC_PARAMS.event_args
 
-# args = EXEC_PARAMS.event_args
-#
-# hl.log_hook(__file__,
-#     {
-#         "cancellable?": str(args.Cancellable),
-#         "doc": str(revit.doc),
-#         "doc_location": str(args.Location),
-#         "options": str(args.Options),
-#     },
-#     log_doc_access=True
-# )
-
-slack_token = 'xoxb-315302503440-2634639326087-Xtjm0FNGFe9K0eAI9ltKTMj4'
+hl.log_hook(__file__,
+    {
+        "cancellable?": str(args.Cancellable),
+        "doc": str(revit.doc),
+        "doc_location": str(args.Location),
+        "options": str(args.Options),
+    },
+    log_doc_access=True
+)
+ 
+slack_token = 'xoxb-315302503440-2634639326087-HsW1TsVCRp9HxCEi07fORbeR'
 client = slack.WebClient(token=slack_token)
 
 def post_message(message_text):
