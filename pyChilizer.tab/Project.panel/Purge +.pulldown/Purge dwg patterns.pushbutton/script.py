48f5ba82d0c85aeab2bbd9ecb3563bad130e7f2e
Deyan Nenov <dnenov@archilizer.com>
2021-11-16 13:04:33 +0000

    Purged purge items :)

--- pyChilizer.tab/Project.panel/Purge +.pulldown/Purge dwg patterns.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/Purge +.pulldown/Purge dwg patterns.pushbutton/script.py
@@ -12,7 +12,8 @@ cl = DB.FilteredElementCollector(doc)
 
 pat_imports = List[DB.ElementId]([i.Id for i in cl.OfClass(DB.LinePatternElement) if "IMPORT" in i.Name])
 
-message = 'There are {} imported line patterns in the model. Are you sure you want to delete them?'.format(str(len(pat_imports)))
+l_num = str(len(pat_imports))
+message = 'There are {} imported line patterns in the model. Are you sure you want to delete them?'.format(l_num)
 
 if len(pat_imports) == 0:
     forms.alert("No Imported Line Patterns, well done!")
@@ -20,6 +21,7 @@ else:
     if forms.alert(message, ok=False, yes=True, no=True, exitscript=True):
         with revit.Transaction("Delete imported patterns"):
             doc.Delete(pat_imports)
+            forms.alert("{} line patterns deleted.".format(l_num), warn_icon=False)
 
 
 
--- pyChilizer.tab/Project.panel/Purge +.pulldown/Purge unused sheets.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/Purge +.pulldown/Purge unused sheets.pushbutton/script.py
@@ -21,7 +21,12 @@ else:
     if forms.alert(message, ok=False, yes=True, no=True, exitscript=True):
         with revit.Transaction("Delete imported patterns"):
             try:
-                doc.Delete(sheets)
+                s_names = [doc.GetElement(s).Title for s in sheets] # Get sheet names  
+                doc.Delete(sheets)  # Delete the sheets 
+                # print result
+                print("SHEETS DELETED:\n")
+                for s in s_names:
+                    print("{}".format(s))
             except:
                 forms.alert("Could not execute the script (make sure there are no active empty Sheets).")
 
