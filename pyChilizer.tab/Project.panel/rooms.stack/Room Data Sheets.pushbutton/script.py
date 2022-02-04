9cd5d477149dd0bacf1b372881a17504ad4c098b
2adicted <dido.nenov@gmail.com>
2022-02-04 21:29:37 +0000

    layouting for room-data-sheets

--- pyChilizer.tab/Project.panel/rooms.stack/Room Data Sheets.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/rooms.stack/Room Data Sheets.pushbutton/script.py
@@ -15,8 +15,9 @@ from pyrevit.framework import List
 import re
 
 output = script.get_output()
-logger = script.get_logger()
-bound_opt = DB.SpatialElementBoundaryOptions()
+logger = script.get_logger()    #helps to debug script, not used
+bound_opt = DB.SpatialElementBoundaryOptions()  #not used?
+
 # use preselected elements, filtering rooms only
 selection = helper.select_rooms_filter()
 if not selection:
@@ -25,13 +26,13 @@ if not selection:
 # TODO: try using room boundaries for crop, if fails use BBox, correct the bbox rotation on fail
 # TODO: move all functions to helper
 
-
 # collect all view templates for plans and sections
 viewsections = DB.FilteredElementCollector(revit.doc).OfClass(DB.ViewSection) # collect sections
 viewsection_dict = {v.Name: v for v in viewsections if v.IsTemplate} # only fetch the IsTemplate sections
 
 viewplans = DB.FilteredElementCollector(revit.doc).OfClass(DB.ViewPlan) # collect plans
 viewplan_dict = {v.Name: v for v in viewplans if v.IsTemplate} # only fetch IsTemplate plans
+
 # add none as an option
 viewplan_dict["<None>"] = None
 viewsection_dict["<None>"] = None
@@ -39,16 +40,15 @@ viewsection_dict["<None>"] = None
 # collect titleblocks in a dictionary
 titleblocks = DB.FilteredElementCollector(revit.doc).OfCategory(DB.BuiltInCategory.OST_TitleBlocks).WhereElementIsElementType()
 tblock_dict = {'{}: {}'.format(tb.FamilyName, revit.query.get_name(tb)): tb for tb in titleblocks}
+tblock_orientation = ['Vertical', 'Horizontal', 'Portrait']
 
 # collect and take the first view plan type, elevation type, set default scale
 col_view_types = (DB.FilteredElementCollector(revit.doc).OfClass(DB.ViewFamilyType).WhereElementIsElementType())
 floor_plan_type = [vt for vt in col_view_types if vt.FamilyName == "Floor Plan"][0]
-# plan_type = DB.ViewType.FloorPlan
 elevation_type = [vt for vt in DB.FilteredElementCollector(revit.doc).OfClass(DB.ViewFamilyType) if vt.FamilyName == "Elevation"][0]
 view_scale = 50
 
 # get units for Crop Offset variable
-
 if helper.is_metric(revit.doc):
     unit_sym = "Crop Offset [mm]"
     default_crop_offset = 350
@@ -67,18 +67,25 @@ components = [
     ComboBox(name="vt_plans", options=sorted(viewplan_dict), default="<None>"),
     Label("View Template for Elevations"),
     ComboBox(name="vt_elevs", options=sorted(viewsection_dict), default="<None>"),
+    Label("Titleblock orientation"),
+    ComboBox(name="tb_orientation", options=tblock_orientation, default="Vertical"),
     Separator(),
     Button("Select"),
 ]
 
 form = FlexForm("Set Sheet Number", components)
 form.show()
+
+# TODO: Catch if user cancelled the form
+
 # match the variables with user input
 chosen_sheet_nr = form.values["sheet_number"]
 chosen_vt_plan = viewplan_dict[form.values["vt_plans"]]
 chosen_vt_elevation = viewsection_dict[form.values["vt_elevs"]]
 chosen_tb = tblock_dict[form.values["tb"]]
 chosen_crop_offset = helper.correct_input_units(form.values["crop_offset"])
+
+# TODO: LAYOUTING OPTIONS
 
 # approximate positions for viewports on an A1 sheet
 plan_position = DB.XYZ(0.282, 0.434, 0)
@@ -184,16 +191,25 @@ for room in selection:
 
         sheet = helper.create_sheet(chosen_sheet_nr, room_name_nr, chosen_tb.Id)
 
+
+    
+    # get positions on sheet
+    poss = helper.get_sheet_pos(sheet, chosen_crop_offset, 4, 3)
+    plan_position = poss[4]
+    elevations_positions = [poss[0], poss[2], poss[6], poss[8]] # a bit hard coded and not pretty at the moment
+
+    print("plan pos: {0}".format(str(plan_position)))
+
     with revit.Transaction("Add Views to Sheet", revit.doc):
         # apply view template
         helper.apply_vt(viewplan, chosen_vt_plan)
         # place view on sheet
         place_plan = DB.Viewport.Create(revit.doc, sheet.Id, viewplan.Id, plan_position)
-        # correct the position, taking Viewport Box Outline as reference
-        delta_pl = plan_position - place_plan.GetBoxOutline().MinimumPoint
-        move_pl = DB.ElementTransformUtils.MoveElement(
-            revit.doc, place_plan.Id, delta_pl
-        )
+        # # correct the position, taking Viewport Box Outline as reference
+        # delta_pl = plan_position - place_plan.GetBoxOutline().MinimumPoint
+        # move_pl = DB.ElementTransformUtils.MoveElement(
+        #     revit.doc, place_plan.Id, delta_pl
+        # )
         for el, pos, i in izip(elevations_col, elevations_positions, elevation_count):
             # place elevations
             place_elevation = DB.Viewport.Create(revit.doc, sheet.Id, el.Id, pos)
@@ -201,17 +217,47 @@ for room in selection:
             place_elevation.get_Parameter(
                 DB.BuiltInParameter.VIEWPORT_DETAIL_NUMBER
             ).Set(i)
-            # correct the positions
-            delta_el = pos - place_elevation.GetBoxOutline().MinimumPoint
-            move_el = DB.ElementTransformUtils.MoveElement(
-                revit.doc, place_elevation.Id, delta_el
-            )
+            # # correct the positions
+            # delta_el = pos - place_elevation.GetBoxOutline().MinimumPoint
+            # move_el = DB.ElementTransformUtils.MoveElement(
+            #     revit.doc, place_elevation.Id, delta_el
+            # )
             revit.doc.Regenerate()
             room_bb = room.get_BoundingBox(el)
             helper.set_crop_to_bb(room, el, crop_offset=chosen_crop_offset)
             helper.apply_vt(el, chosen_vt_elevation)
 
         revit.doc.Regenerate()
+
+
+    # with revit.Transaction("Add Views to Sheet", revit.doc):
+    #     # apply view template
+    #     helper.apply_vt(viewplan, chosen_vt_plan)
+    #     # place view on sheet
+    #     place_plan = DB.Viewport.Create(revit.doc, sheet.Id, viewplan.Id, plan_position)
+    #     # correct the position, taking Viewport Box Outline as reference
+    #     delta_pl = plan_position - place_plan.GetBoxOutline().MinimumPoint
+    #     move_pl = DB.ElementTransformUtils.MoveElement(
+    #         revit.doc, place_plan.Id, delta_pl
+    #     )
+    #     for el, pos, i in izip(elevations_col, elevations_positions, elevation_count):
+    #         # place elevations
+    #         place_elevation = DB.Viewport.Create(revit.doc, sheet.Id, el.Id, pos)
+    #         # set viewport detail number
+    #         place_elevation.get_Parameter(
+    #             DB.BuiltInParameter.VIEWPORT_DETAIL_NUMBER
+    #         ).Set(i)
+    #         # correct the positions
+    #         delta_el = pos - place_elevation.GetBoxOutline().MinimumPoint
+    #         move_el = DB.ElementTransformUtils.MoveElement(
+    #             revit.doc, place_elevation.Id, delta_el
+    #         )
+    #         revit.doc.Regenerate()
+    #         room_bb = room.get_BoundingBox(el)
+    #         helper.set_crop_to_bb(room, el, crop_offset=chosen_crop_offset)
+    #         helper.apply_vt(el, chosen_vt_elevation)
+
+    #     revit.doc.Regenerate()
 
 
         print("Sheet : {0} \t Room {1} ".format(output.linkify(sheet.Id), room_name_nr))
