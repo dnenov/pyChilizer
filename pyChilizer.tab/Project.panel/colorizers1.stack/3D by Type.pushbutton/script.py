f795a8dad9164342e5cd02df5728ab20dc60ccb3
rastermadre <daria.ivanciucova@gmail.com>
2023-05-04 11:57:20 +0100

    updated colorize 3D by Type
    - fixed view name for CWMullions and CWPanels
    - sorted cats
    - added more popular categories to the list
    - hid the walls that are not CW (using hide element in view)

--- pyChilizer.tab/Project.panel/colorizers1.stack/3D by Type.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/colorizers1.stack/3D by Type.pushbutton/script.py
@@ -6,6 +6,8 @@ from pyrevit import script
 import random
 import threedconfig
 from pychilizer import database, colorize
+from pyrevit.framework import List
+
 
 logger = script.get_logger()
 BIC = DB.BuiltInCategory
@@ -18,9 +20,9 @@ solid_fill_pattern = database.get_solid_fill_pat(doc=doc)
 # [x] revise colours to exclude nearby colours
 # [x] include more categories
 # [x] set view to open active
-# [ ] include which types to colorize
-# [ ] test in R2022 R2023
-# [ ] work with links?
+# [-] include which types to colorize - unclear if useful
+# [x] test in R2022 R2023
+# [-] work with links? - cannot override
 # [x] fix dependency on the initial 3D view
 
 
@@ -33,19 +35,32 @@ category_opt_dict = {
     "Casework": BIC.OST_Casework,
     "Furniture": BIC.OST_Furniture,
     "Furniture Systems": BIC.OST_FurnitureSystems,
+    "Electrical Equipment":BIC.OST_ElectricalEquipment,
+    "Electrical Fixtures":BIC.OST_ElectricalFixtures,
+    "Parking":BIC.OST_Parking,
+    "Site":BIC.OST_Site,
+    "Entourage":BIC.OST_Entourage,
     "Plumbing Fixtures": BIC.OST_PlumbingFixtures,
     "Roofs": BIC.OST_Roofs,
     "Specialty Equipment": BIC.OST_SpecialityEquipment,
     "Ceilings": BIC.OST_Ceilings,
-    "Curtain Wall Panels": BIC.OST_CurtainWallPanels
-}
+    "Curtain Wall Panels": BIC.OST_CurtainWallPanels,
+    "Curtain Wall Mullions": BIC.OST_CurtainWallMullions,
+    "Topography":BIC.OST_Topography,
+    "Structural Columns":BIC.OST_StructuralColumns,
+    "Structural Framing":BIC.OST_StructuralFraming,
+    "Stairs":BIC.OST_Stairs,
+    "Ramps":BIC.OST_Ramps,
 
-selected_cat = forms.CommandSwitchWindow.show(category_opt_dict, message="Select Category to Colorize", width = 400)
+}
+sorted_cats = sorted(category_opt_dict.keys(), key=lambda x:x)
+
+selected_cat = forms.CommandSwitchWindow.show(sorted_cats, message="Select Category to Colorize", width = 400)
 if selected_cat == None:
     script.exit()
 
 chosen_bic = [category_opt_dict[selected_cat]]
-if selected_cat == "Curtain Wall Panels": # not so elegant way to support curtain panels by adding walls category
+if selected_cat in ["Curtain Wall Panels", "Curtain Wall Mullions"]: # not so elegant way to support curtain panels by adding walls category
     chosen_bic.append(BIC.OST_Walls)
 
 # get all element categories and return a list of all categories except chosen BIC
@@ -54,8 +69,7 @@ chosen_category = [all_cats.get_Item(i) for i in chosen_bic]
 hide_categories_except = [c for c in all_cats if c.Id not in [i.Id for i in chosen_category]]
 
 with revit.Transaction("Create Colorized 3D"):
-    for cc in chosen_category:
-        view_name = "Colorize {} by Type".format(cc.Name)
+    view_name = "Colorize {} by Type".format(selected_cat)
     if database.delete_existing_view(view_name, doc=doc):
         # create new 3D
         viewtype_id = database.get_3Dviewtype_id(doc=doc)
@@ -68,25 +82,34 @@ with revit.Transaction("Create Colorized 3D"):
         if view.CanCategoryBeHidden(cat.Id):
             view.SetCategoryHidden(cat.Id, True)
 
-    get_view_elements = [DB.FilteredElementCollector(doc) \
-        .OfCategory(cb) \
+    get_view_elements = DB.FilteredElementCollector(doc) \
+        .OfCategory(chosen_bic[0]) \
         .WhereElementIsNotElementType() \
-        .ToElements() for cb in chosen_bic 
-        ]
+        .ToElements()
+
+    # for special cases with Curtain Wall Mullions or Curtain View Panel - hide walls that are not Panels
+    if selected_cat in ["Curtain Wall Panels", "Curtain Wall Mullions"]:
+        # get all walls that are not panels (their category will be == Walls)
+        get_wall_elements = DB.FilteredElementCollector(doc)\
+                            .OfCategory(BIC.OST_Walls) \
+                            .WhereElementIsNotElementType() \
+                                .ToElements()
+        # filter the walls that are not Curtain Walls and hide them
+        not_cw_elements = List[DB.ElementId]([w.Id for w in get_wall_elements if w.WallType.Kind != DB.WallKind.Curtain ])
+        view.HideElements(not_cw_elements)
 
 types_dict = defaultdict(set)
-for vl in get_view_elements:
-    for el in vl:
-        # discard nested shared - group under the parent family
-        if selected_cat in ["Floors", "Walls", "Roofs", "Ceilings"]:
-            type_id = el.GetTypeId()
-        else:
-            try:
-                type_id = el.SuperComponent.GetTypeId()
-            except:
-                type_id = el.GetTypeId()
-        types_dict[type_id].add(el.Id)
+for el in get_view_elements:
 
+    # discard nested shared - group under the parent family
+    if selected_cat in ["Floors", "Walls", "Roofs", "Ceilings"]:
+        type_id = el.GetTypeId()
+    else:
+        try:
+            type_id = el.SuperComponent.GetTypeId()
+        except:
+            type_id = el.GetTypeId()
+    types_dict[type_id].add(el.Id)
 
 # colour dictionary
 n = len(types_dict)
