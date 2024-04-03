784e0afe73d2afd7f85d9d742d51a723b5d766a5
dariaivanciucova <divanciucova@archilizer.com>
2024-04-03 11:20:12 +0100

    fixed bugs with BIP for Colorize by value

--- pyChilizer.tab/Project.panel/colorizers2.stack/Colorize by Value.pushbutton/script.py
+++ pyChilizer.tab/Project.panel/colorizers2.stack/Colorize by Value.pushbutton/script.py
@@ -29,6 +29,21 @@ class ParameterOption(forms.TemplateListItem):
         return str(self.param_dict[self.item])
 
 
+def get_element_parameter(el, sel_parameter):
+    try:
+        parameter_element = doc.GetElement(sel_parameter)
+    except TypeError:
+        el_parameter = el.get_Parameter(sel_parameter)
+        return el_parameter
+    if isinstance(parameter_element, DB.SharedParameterElement):
+        # if shared parameter
+        el_parameter = el.get_Parameter(parameter_element.GuidValue)
+    else:
+        # if builtin parameter
+        el_parameter = el.get_Parameter(parameter_element)
+    return el_parameter
+
+
 sorted_cats = sorted(categories_for_selection.keys(), key=lambda x: x)
 
 if forms.check_modelview(revit.active_view):
@@ -66,8 +81,10 @@ for e in get_view_elements:
             pretty_param_name = "".join([str(ip.Definition.Name), " [Shared Parameter]"])
             inst_param_dict[ip.Definition.Id] = pretty_param_name
         # if the param is BIP - store as BIP
+
         elif param_is_bip(ip) and ip.Definition.Name not in inst_param_dict:
             inst_param_dict[ip.Definition.BuiltInParameter] = str(ip.Definition.Name)
+
         # another case - Project Parameters - NOT SUPPORTED due to possible duplicates with Family Parameters
         # elif not (ip.IsShared) and not(param_is_bip(ip)) and ip.Definition.Id not in inst_param_dict:
         #     pretty_param_name = "".join([str(ip.Definition.Name), " [Project Parameter]"])
@@ -85,7 +102,6 @@ for e in get_view_elements:
         #     inst_param_dict[tp.Definition.Id] = pretty_param_name
 
 # show UI form to pick parameters
-# todo: clean this
 instance_p_class = [ParameterOption(x, inst_param_dict) for x in inst_param_dict.keys()]
 type_p_class = [ParameterOption(x, type_param_dict) for x in type_param_dict.keys()]
 i_p_ops = sorted(instance_p_class, key=lambda x: x.name)
@@ -98,7 +114,6 @@ selected_parameter = forms.SelectFromList.show(ops,
                                                multiselect=False)
 
 forms.alert_ifnot(selected_parameter, "No Parameters Selected", exitscript=True)
-
 # get elements in current view
 first_el = get_view_elements[0]
 
@@ -106,25 +121,29 @@ first_el = get_view_elements[0]
 values_dict = defaultdict(list)  # {value of parameter : element id}
 
 for el in get_view_elements:
-    if selected_parameter in inst_param_dict.keys():
-        el_parameter = el.get_Parameter(selected_parameter)
-        if el_parameter:
-            param_value = database.get_param_value_as_string(el_parameter)
-            # values_dict[param_value] = []
-            values_dict[param_value].append(el.Id)
+    if selected_parameter in inst_param_dict.keys():  # if instance parameter
+        el_parameter = get_element_parameter(el, selected_parameter)
     else:
-        el_type = query.get_type(el)
-        element_type_parameter = el_type.get_Parameter(selected_parameter)
-        if element_type_parameter:
-            param_value = database.get_param_value_as_string(element_type_parameter)
-            values_dict[param_value].append(el.Id)
+        el_type = query.get_type(el)  # if type parameter
+        try: # case when it's a shared parameter
+            parameter_element = doc.GetElement(selected_parameter)
+        except TypeError:
+            parameter_element = selected_parameter
+        if isinstance(parameter_element, DB.SharedParameterElement):
+            #         # if shared parameter
+            el_parameter = el_type.get_Parameter(parameter_element.GuidValue)
+        else:
+            # if builtin parameter
+            el_parameter = el_type.get_Parameter(parameter_element)
+
+    if el_parameter:
+        param_value = database.get_param_value_as_string(el_parameter)
+        values_dict[param_value].append(el.Id)
+
 
 # colour dictionary
 n = len(values_dict.keys())
 revit_colours = colorize.get_colours(n)
-
-override_filters = 0
-
 with revit.Transaction("Colorize by Value", doc):
     for param_value, colour in zip(values_dict.keys(), revit_colours):
         override = colorize.set_colour_overrides_by_option(overrides_option, colour, doc)
